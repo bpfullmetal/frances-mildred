b8204d8daf4477aabaf2dd1ae83c716c9527d1df
lightfury.dev <blacktiny126@hotmail.com>
2023-12-14 02:10:11 -0500

    build the contact page

---
+++ src/components/contact/index.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import ContactBannerImage from '../../assets/images/contact-banner.png';
+import LogoIcon from '../../assets/images/icon.png';
+import LetterA from '../../assets/js/icons/letter-a';
+import LetterC from '../../assets/js/icons/letter-c';
+import LetterD from '../../assets/js/icons/letter-d';
+import LetterE from '../../assets/js/icons/letter-e';
+import LetterF from '../../assets/js/icons/letter-f';
+import LetterI from '../../assets/js/icons/letter-i';
+import LetterL from '../../assets/js/icons/letter-l';
+import LetterM from '../../assets/js/icons/letter-m';
+import LetterN from '../../assets/js/icons/letter-n';
+import LetterR from '../../assets/js/icons/letter-r';
+import LetterS from '../../assets/js/icons/letter-s';
+import HeaderMenu from '../header-menu';
+
+const ContactPageContent = ({ content, menuItems, title, uri }) => {
+  return (
+    <main className="h-screen flex flex-col">
+      <HeaderMenu menuItems={menuItems} currentURI={uri} />
+
+      <footer className="relative flex-1">
+        <div className="absolute w-full h-full">
+          <img
+            className="w-full h-full rounded-none object-cover"
+            src={ContactBannerImage}
+            alt="contact banner"
+          />
+        </div>
+
+        <div className="relative h-full flex flex-col justify-between max-w-main mx-auto px-5 sm:px-12">
+          <div className="flex justify-between py-20">
+            <div className="flex flex-col mr-10">
+              <div className="flex flex-col space-y-4 sm:flex-row sm:space-x-14 sm:space-y-0">
+                <div className="flex flex-col">
+                  <p>94 Prince St.3rd Floor</p>
+                  <p>New York, NY, 10012</p>
+                </div>
+                <div className="flex flex-col">
+                  <a href="tel:+1 212 981 4599">+1 212 981 4599</a>
+                  <a href="mailto:info@francesmildred.com">
+                    info@francesmildred.com
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-1 flex-col items-end">
+              <img className="w-14 h-14" src={LogoIcon} alt="logo" />
+            </div>
+          </div>
+
+          <div className="logo flex flex-col mb-9 space-y-6 sm:mb-12 sm:space-y-24">
+            <div className="flex justify-between">
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterF />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterR />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterA />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterN />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterC />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterE />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterS />
+              </div>
+            </div>
+            <div className="flex justify-between">
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterM />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterI />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterL />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterD />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterR />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterE />
+              </div>
+              <div className="h-8 sm:h-16 md:h-24 lg:h-28">
+                <LetterD />
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </main>
+  );
+};
+
+export default ContactPageContent;
